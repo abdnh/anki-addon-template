27f767401a59958a42809f9fa85046725eaebd94
Abdo <abdo@abdnh.net>
2023-09-21 23:06:32 +0300

    Add pytest guard

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,4 @@
-import os
 import sys
 
-from aqt.utils import showInfo
-
-sys.path.append(os.path.join(os.path.dirname(__file__), "vendor"))
-
-from .consts import consts
-
-showInfo(f"Hello from {consts.name}!")
+if "pytest" not in sys.modules:
+    from . import main
---
+++ src/main.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+from aqt.qt import *
+from aqt.utils import showInfo
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "vendor"))
+
+from .consts import consts
+
+showInfo(f"Hello from {consts.name}!")
