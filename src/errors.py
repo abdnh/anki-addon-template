f095e810ffc7423f48526e28d768854cf62bab7d
Abdo <abdo@abdnh.net>
2024-05-21 19:55:41 +0300

    Fix RuntimeError on shutdown

--- src/errors.py
+++ src/errors.py
@@ -1,4 +1,5 @@
-from aqt import gui_hooks
+from anki.hooks import wrap
+from aqt import gui_hooks, mw
 from aqt.qt import *
 
 from .config import config
@@ -15,5 +16,15 @@ def _on_profile_did_open() -> None:
         logger.warning("ankiutils.errors not found; error handling is disabled.")
 
 
+def _before_exit() -> None:
+    import atexit
+    import logging
+
+    # Fix 'RuntimeError: wrapped C/C++ object of type ErrorHandler has been deleted' on shutdown
+    atexit.unregister(logging.shutdown)
+    logging.shutdown()
+
+
 def setup_error_handler() -> None:
     gui_hooks.profile_did_open.append(_on_profile_did_open)
+    mw.cleanupAndExit = wrap(mw.cleanupAndExit, _before_exit, "before")  # type: ignore
