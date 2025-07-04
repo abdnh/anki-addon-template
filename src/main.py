8dea295f0d94474724aec00d8fad957d4350580a
Abdo <abdo@abdnh.net>
2025-07-04 09:35:04 +0300

    Patch certifi.where()

--- src/main.py
+++ src/main.py
@@ -1,5 +1,10 @@
 from aqt.utils import showInfo
 
+from .patches import patch_certifi
+
+patch_certifi()
+
+# ruff: noqa: E402
 from .consts import consts
 from .errors import setup_error_handler
 from .log import logger
