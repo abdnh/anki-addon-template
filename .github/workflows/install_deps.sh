a55ba15e866e3fb4ccab5b84e2c998d8396f26a7
Abdo <abdo@abdnh.net>
2025-07-09 21:02:35 +0300

    Use astral-sh/setup-uv action

--- .github/workflows/install_deps.sh
+++ .github/workflows/install_deps.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-pip install uv
-uv sync
+uv sync --locked --dev
 sudo apt-get update
 sudo apt install libxcb-xinerama0 libxcb-cursor0 libegl1
 curl -fsSL https://dprint.dev/install.sh | sh -s > /dev/null 2>&1
