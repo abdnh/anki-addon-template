3e71fa25d80e13b5f7afb9812b3e036524f9ff50
Abdo <abdo@abdnh.net>
2025-08-07 05:23:14 +0300

    Add demo menu

--- src/main.py
+++ src/main.py
@@ -1,17 +1,27 @@
-from aqt.utils import showInfo
-
 from .patches import patch_certifi
 
 patch_certifi()
 
 # ruff: noqa: E402
+from aqt import QMenu, mw
+from aqt.utils import showInfo
+
 from .consts import consts
 from .errors import setup_error_handler
 from .log import logger
 
 
+def on_action() -> None:
+    showInfo("Hello world!")
+
+
+def add_menu() -> None:
+    menu = QMenu(consts.name, mw)
+    menu.addAction("Hello", on_action)
+    mw.form.menuTools.addMenu(menu)
+
+
 def init() -> None:
     setup_error_handler()
-    msg = f"Hello from {consts.name}!"
-    logger.debug(msg)
-    showInfo(msg)
+    logger.debug("Hello world!", addon=consts.name)
+    add_menu()
