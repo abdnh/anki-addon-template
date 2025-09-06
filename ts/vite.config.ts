30d8a86eddc15a55d613b565a539008ae212448c
Abdo <abdo@abdnh.net>
2025-09-07 00:58:29 +0300

    Remove playwright

--- ts/vite.config.ts
+++ ts/vite.config.ts
@@ -9,21 +9,6 @@ export default defineConfig({
             {
                 extends: "./vite.config.ts",
                 test: {
-                    name: "client",
-                    environment: "browser",
-                    browser: {
-                        enabled: true,
-                        provider: "playwright",
-                        instances: [{ browser: "chromium" }],
-                    },
-                    include: ["src/**/*.svelte.{test,spec}.{js,ts}"],
-                    exclude: ["src/lib/server/**"],
-                    setupFiles: ["./vitest-setup-client.ts"],
-                },
-            },
-            {
-                extends: "./vite.config.ts",
-                test: {
                     name: "server",
                     environment: "node",
                     include: ["src/**/*.{test,spec}.{js,ts}"],
