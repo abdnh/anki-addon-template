d1e55eda44532142cc216969c1cbe9a7971b2e66
Abdo <abdo@abdnh.net>
2025-09-06 22:00:06 +0300

    Set default title of Dialog

--- src/gui/dialog.py
+++ src/gui/dialog.py
@@ -2,6 +2,7 @@ from typing import Optional
 
 from aqt.qt import Qt, QWidget
 
+from ..consts import consts
 from ..vendor.ankiutils.gui import dialog
 
 
@@ -10,5 +11,14 @@ class Dialog(dialog.Dialog):
         self,
         parent: Optional[QWidget] = None,
         flags: Qt.WindowType = Qt.WindowType.Dialog,
+        subtitle: str = "",
     ) -> None:
+        self.subtitle = subtitle
         super().__init__(__name__, parent, flags)
+
+    def setup_ui(self) -> None:
+        super().setup_ui()
+        title = consts.name
+        if self.subtitle:
+            title += f" - {self.subtitle}"
+        self.setWindowTitle(title)
